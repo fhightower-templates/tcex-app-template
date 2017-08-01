b1328408fb8c1627c09edf900bfb9e5dc19032c0
Floyd Hightower <floyd.hightower27@gmail.com>
2017-08-01 10:51:52 -0400

    Adding setup.py
    Fixes #1

---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/setup.py
@@ -0,0 +1,21 @@
+import json
+from setuptools import setup, find_packages
+
+with open('install.json', 'r') as fh:
+    version = json.load(fh)['programVersion']
+
+if not version:
+    raise RuntimeError('Cannot find version information')
+
+setup(
+    author='{{ cookiecutter.author_name }}',
+    description='{{ cookiecutter.project_description }}',
+    install_requires=[
+        # TODO: Add required packages here...
+        'tcex>=0.3,<0.4'
+    ],
+    license='{{ cookiecutter.open_source_license }}',
+    name='{{ cookiecutter.project_slug }}',
+    packages=find_packages(),
+    version=version
+)
