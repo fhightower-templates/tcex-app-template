b9b6e1ce3221677310597c4b23ea94dc2ec7c2f9
Floyd Hightower <floyd.hightower27@gmail.com>
2018-02-22 11:36:19 -0500

    Updating the make file and tcex files

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ update:  # update the file from the main tcex repo
 	# clone the tcex repo
 	git clone https://github.com/ThreatConnect-Inc/tcex;
 	# copy app.py
-	cp ./tcex/tcex/app.py ./\{\{cookiecutter.project_slug\}\}/\{\{cookiecutter.project_slug\}\}/
+	# cp ./tcex/tcex/app.py ./\{\{cookiecutter.project_slug\}\}/\{\{cookiecutter.project_slug\}\}/
 	# copy __main__.py
 	cp ./tcex/tcex/__main__.py ./\{\{cookiecutter.project_slug\}\}/\{\{cookiecutter.project_slug\}\}/
 	# copy tcex_json_schema.json
--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__main__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__main__.py
@@ -1,8 +1,13 @@
+"""Set lib directory for current version of Python"""
 import os
 import subprocess
 import sys
 
+__version__ = '1.0.1'
+
+
 def main():
+    """Main"""
     lib_directory = None
 
     # All Python Version that will be searched
@@ -18,8 +23,11 @@ def main():
         # ensure content starts with lib, is directory, and is readable
         if c.startswith('lib') and os.path.isdir(c) and (os.access(c, os.R_OK)):
             lib_directories.append(c)
+    # reverse sort directories
+    lib_directories.sort(reverse=True)
 
     # Find most appropriate FULL version
+    lib_directory = None
     if lib_micro_version in lib_directories:
         lib_directory = lib_micro_version
     elif lib_minor_version in lib_directories:
@@ -27,14 +35,14 @@ def main():
     elif lib_major_version in lib_directories:
         lib_directory = lib_major_version
     else:
-        # file most appropriate PARTIAL version
-        for ld in lib_directories:
-            if lib_micro_version in ld:
-                lib_directory = ld
-            elif lib_minor_version in ld:
-                lib_directory = ld
-            elif lib_major_version in ld:
-                lib_directory = ld
+        for lv in [lib_micro_version, lib_minor_version, lib_major_version]:
+            for ld in lib_directories:
+                if lv in ld:
+                    lib_directory = ld
+                    break
+            else:
+                continue
+            break
 
     # No reason to continue if no valid lib directory found
     if lib_directory is None:
@@ -42,7 +50,10 @@ def main():
         sys.exit(1)
 
     # Use this if you want to include modules from a subfolder
-    # lib_path = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe()))[0], lib_directory)))
+    # lib_path = os.path.realpath(
+    #     os.path.abspath(
+    #         os.path.join(
+    #             os.path.split(inspect.getfile(inspect.currentframe()))[0], lib_directory)))
     lib_path = os.path.join(app_path, lib_directory)
     if 'PYTHONPATH' in os.environ:
         os.environ['PYTHONPATH'] = '{}{}{}'.format(lib_path, os.pathsep, os.environ['PYTHONPATH'])
@@ -56,6 +67,7 @@ def main():
     # Make sure to exit with the return value from the subprocess call
     ret = subprocess.call(sys.argv)
     sys.exit(ret)
+
 
 if __name__ == '__main__':
     main()
