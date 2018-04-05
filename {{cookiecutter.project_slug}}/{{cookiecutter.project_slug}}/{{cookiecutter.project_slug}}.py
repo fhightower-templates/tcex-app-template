ee25aa25e5f9b5aba497ed8f3d95d462197fcac6
Floyd Hightower <floyd.hightower27@gmail.com>
2018-04-05 11:21:39 -0400

    Fixing layout and improving argument handling for playbook apps vs jobs apps
    Fixes #16

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.py
@@ -4,18 +4,20 @@
 import traceback
 
 from tcex import TcEx
+{%- if cookiecutter.runtime_level == 'Playbook' %}
 
 
 def parse_arguments():
     """Parse arguments coming into the app."""
-    {% if cookiecutter.runtime_level == 'Playbook' %}tcex.parser.add_argument('--string', help='Input string', required=True){% elif cookiecutter.runtime_level == 'Organization' %}tcex.parser.add_argument('--api_default_org', help='Default owner', required=True){% endif %}
+    tcex.parser.add_argument('--string', help='Input string', required=True)
     return tcex.args
+{%- endif %}
 
 
 def main():
     """."""
     args = parse_arguments()
-    {% if cookiecutter.runtime_level == 'Playbook' %}
+    {% if cookiecutter.runtime_level == 'Playbook' -%}
     # read the string from the playbook to get the actual value of the argument
     string = tcex.playbook.read(args.string)
 
@@ -23,14 +25,15 @@ def main():
 
     # output the reversed string to downstream playbook apps
     tcex.playbook.create_output('{{cookiecutter.project_slug}}.reversed_string', string[::-1])
-    {% elif cookiecutter.runtime_level == 'Organization' %}
+    {%- elif cookiecutter.runtime_level == 'Organization' -%}
     tcex.log.info('Hello, world!')
-    {% endif %}
+    {%- endif %}
     tcex.exit(0)
 
 
 if __name__ == "__main__":
-    tcex = TcEx()
+    tcex = TcEx(){% if cookiecutter.runtime_level == 'Organization' %}
+    args = tcex.args{% endif %}
     try:
         # start the app
         main()
