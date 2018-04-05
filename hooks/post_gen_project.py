6e2fe29678c42df79b1e3264181e41103cc68a6e
Floyd Hightower <floyd.hightower27@gmail.com>
2018-04-05 11:54:48 -0400

    Updating files from the tcex repo when creating a new project
    Fixes #14

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
+
 import os
+
+try:
+    import requests
+except ModuleNotFoundError:
+    make_request = False
+else:
+    make_request = True
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
@@ -8,6 +16,29 @@ def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
 
 
+def _make_request(url):
+    r = requests.get(url)
+
+    if r.ok:
+        return r.text
+    else:
+        print('Unable to update __main__.py because of a {} response from {}: {}'.format(r.status_code, url, r.text))
+        return None
+
+
+def update_file(url, file):
+    """Update the given file with the content from a given url."""
+    content = _make_request(url)
+
+    if content is not None:
+        with open(os.path.join(PROJECT_DIRECTORY, '{{ cookiecutter.project_slug }}/{}'.format(file)), 'w') as f:
+            f.write(content)
+
+
 if __name__ == '__main__':
     if 'Not open source' == '{{ cookiecutter.open_source_license }}':
         remove_file('LICENSE')
+
+    if make_request:
+        update_file('https://raw.githubusercontent.com/ThreatConnect-Inc/tcex/master/tcex/__main__.py', '__main__.py')
+        update_file('https://raw.githubusercontent.com/ThreatConnect-Inc/tcex/master/tcex/tcex_json_schema.json', 'tcex_json_schema.json')
